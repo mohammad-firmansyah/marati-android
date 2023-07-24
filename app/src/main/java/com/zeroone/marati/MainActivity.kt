b68f8922b307771efeb285bfbe22b649ff3ef7be
mohammad firmansyah <firmansmoh842@gmail.com>
2023-07-24 19:40:48 +0700

    Initial commit

---
+++ app/src/main/java/com/zeroone/marati/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.zeroone.marati
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zeroone.marati.ui.OnboardingActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this@MainActivity,OnboardingActivity::class.java))
+    }
+}
---
+++ app/src/main/java/com/zeroone/marati/ui/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.zeroone.marati.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zeroone.marati.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
