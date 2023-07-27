361524c1c8a5b04ef8ee234e11de9079e8d17b2a
mohammad firmansyah <firmansmoh842@gmail.com>
2023-07-27 12:20:38 +0700

    add animation

--- app/src/main/java/com/zeroone/marati/ui/Onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/zeroone/marati/ui/Onboarding/OnboardingActivity.kt
@@ -1,8 +1,10 @@
 package com.zeroone.marati.ui.Onboarding
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.zeroone.marati.R
 import com.zeroone.marati.databinding.ActivityOnboardingBinding
 import com.zeroone.marati.ui.Login.LoginActivity
@@ -19,6 +21,9 @@ class OnboardingActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
 
+        playAnimation()
+
+
 
         binding.loginBtn.setOnClickListener {
             startActivity(Intent(this@OnboardingActivity,LoginActivity::class.java))
@@ -27,6 +32,29 @@ class OnboardingActivity : AppCompatActivity() {
         binding.registerBtn.setOnClickListener {
             startActivity(Intent(this@OnboardingActivity,RegisterActivity::class.java))
         }
+
+    }
+
+    fun playAnimation(){
+        ObjectAnimator.ofFloat(binding.textView, View.TRANSLATION_X, -60f, 0f).apply {
+            duration = 600
+            repeatCount = 0
+        }.start()
+
+        ObjectAnimator.ofFloat(binding.textView2, View.TRANSLATION_X, -60f, 0f).apply {
+            duration = 800
+            repeatCount = 0
+        }.start()
+
+        ObjectAnimator.ofFloat(binding.loginBtn, View.TRANSLATION_X, -60f, 0f).apply {
+            duration = 1000
+            repeatCount = 0
+        }.start()
+
+        ObjectAnimator.ofFloat(binding.registerBtn, View.TRANSLATION_X, -60f, 0f).apply {
+            duration = 1200
+            repeatCount = 0
+        }.start()
 
     }
 }
