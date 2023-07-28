390f5cb59232b820d42782a6982c15a7ce20694b
mohammad firmansyah <firmansmoh842@gmail.com>
2023-07-28 16:32:00 +0700

    add home page

--- app/src/main/java/com/zeroone/marati/ui/Login/LoginActivity.kt
+++ app/src/main/java/com/zeroone/marati/ui/Login/LoginActivity.kt
@@ -1,13 +1,23 @@
 package com.zeroone.marati.ui.Login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.zeroone.marati.R
+import com.zeroone.marati.databinding.ActivityLoginBinding
+import com.zeroone.marati.ui.Home.HomeActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
+
+        binding.button.setOnClickListener{
+            startActivity(Intent(this@LoginActivity,HomeActivity::class.java))
+        }
     }
 }
