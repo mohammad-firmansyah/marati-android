0776f9df6e3edada8273c10f32f0981edf44c409
mohammad firmansyah <firmansmoh842@gmail.com>
2024-03-03 18:02:21 +0700

    add : singleton get instance

--- app/src/main/java/com/zeroone/marati/core/ui/ViewModelFactory.kt
+++ app/src/main/java/com/zeroone/marati/core/ui/ViewModelFactory.kt
@@ -1,6 +1,32 @@
 package com.zeroone.marati.core.ui
 
 import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.zeroone.marati.Login.LoginViewModel
 
-class ViewModelFactory(context: Context) {
+
+class ViewModelFactory(private val context: Context,private val pref: PreferenceManager) : ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
+                LoginViewModel(pref) as T
+            }
+
+
+            else -> throw Throwable("Unknown ViewModel class: " + modelClass.name)
+        }
+
+    }
+
+    companion object {
+        @Volatile
+        private var instance: ViewModelFactory? = null
+        fun getInstance(context: Context,pref:PreferenceManager): ViewModelFactory =
+            instance ?: synchronized(this) {
+                instance ?: ViewModelFactory(context, pref)
+            }.also { instance = it }
+    }
 }
+
