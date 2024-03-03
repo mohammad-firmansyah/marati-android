6bde627e6a08b5c7de9183ae9f166adcbe1d8f49
mohammad firmansyah <firmansmoh842@gmail.com>
2024-03-03 18:01:27 +0700

    add : view model

---
+++ app/src/main/java/com/zeroone/marati/Login/LoginViewModel.kt
@@ -0,0 +1,94 @@
+package com.zeroone.marati.Login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
+import com.zeroone.marati.core.data.source.remote.response.Data
+import com.zeroone.marati.core.data.source.remote.response.LoginResponse
+import com.zeroone.marati.core.data.source.remote.retrofit.ApiConfig
+import com.zeroone.marati.core.ui.PreferenceManager
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class LoginViewModel(private val pref:PreferenceManager) : ViewModel() {
+    val _user : MutableLiveData<Data> = MutableLiveData<Data>()
+    val user : LiveData<Data> = _user
+
+    val _errorMessage : MutableLiveData<String> = MutableLiveData<String>()
+    val errorMessage : LiveData<String> = _errorMessage
+
+    fun setToken(token: String?){
+        viewModelScope.launch {
+            pref.setToken(token)
+        }
+    }
+
+    fun getToken():String{
+        return runBlocking {
+            pref.getToken().first()
+        }
+    }
+
+    fun login(email:String,password:String){
+        val body = JSONObject()
+        body.put("email",email)
+        body.put("password",password)
+        val requestBody = body.toString().toRequestBody("application/json".toMediaTypeOrNull())
+        val client = ApiConfig.provideApiServiceJs().login(requestBody)
+        client.enqueue(object : Callback<LoginResponse> {
+            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                try {
+                    if(response.isSuccessful){
+                        _user.value = response.body()?.data!!
+                    } else{
+                        val errorBody = response.errorBody()?.string()
+                        val errorResponse = Gson().fromJson(errorBody,LoginResponse::class.java)
+                        val errorMessage = errorResponse.message
+                        _errorMessage.value = errorMessage
+                    }
+                }catch (e:Exception){
+                    _errorMessage.value = e.message
+                }
+            }
+
+            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                _errorMessage.value = t.message.toString()
+            }
+
+        })
+    }
+
+    fun socialLogin(token:String){
+        val body = JSONObject()
+        body.put("token",token)
+        val requestBody = body.toString().toRequestBody("application/json".toMediaTypeOrNull())
+        val client = ApiConfig.provideApiServiceJs().socialLogin(requestBody)
+        client.enqueue(object : Callback<LoginResponse> {
+            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                try {
+                    if(response.isSuccessful){
+                        _user.value = response.body()?.data!!
+                    } else{
+                        _errorMessage.value = response.body()?.message!!
+                    }
+                }catch (e:Exception){
+                    _errorMessage.value = e.message
+                }
+            }
+
+            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                _errorMessage.value = t.message.toString()
+            }
+
+        })
+    }
+}
