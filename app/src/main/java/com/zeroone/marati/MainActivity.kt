86afdf958e72489b34892577908e8353a6ad42a0
mohammad firmansyah <firmansmoh842@gmail.com>
2024-03-03 18:04:02 +0700

    add : initialization of data store

--- app/src/main/java/com/zeroone/marati/MainActivity.kt
+++ app/src/main/java/com/zeroone/marati/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.zeroone.marati
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Build
@@ -7,8 +8,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.zeroone.marati.Onboarding.OnboardingActivity
 import com.zeroone.marati.Splashscreen.SplashscreenActivity
+
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "session")
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
