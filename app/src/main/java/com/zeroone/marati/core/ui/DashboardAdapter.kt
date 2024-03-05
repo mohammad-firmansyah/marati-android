4ea281ef69be443b906e6c9c2e72d281f96fc872
mohammad firmansyah <firmansmoh842@gmail.com>
2024-03-05 23:19:56 +0700

    add : delete confirmation

--- app/src/main/java/com/zeroone/marati/core/ui/DashboardAdapter.kt
+++ app/src/main/java/com/zeroone/marati/core/ui/DashboardAdapter.kt
@@ -1,21 +1,28 @@
 package com.zeroone.marati.core.ui
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.zeroone.marati.Home.HomeViewModel
 import com.zeroone.marati.R
 import com.zeroone.marati.core.data.source.remote.response.DataItem
+import com.zeroone.marati.core.utils.Utils
 
-class DashboardAdapter(private var list:List<DataItem>?) : RecyclerView.Adapter<DashboardAdapter.ViewHolder>() {
+class DashboardAdapter(
+    private val context : Context,
+    private var list:List<DataItem>?,
+    private val viewModel:HomeViewModel) : RecyclerView.Adapter<DashboardAdapter.ViewHolder>() {
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
         val deleteProject: ImageButton = itemView.findViewById(R.id.deleteProject)
         val title: TextView = itemView.findViewById(R.id.title)
         val created_date: TextView = itemView.findViewById(R.id.created_date)
         val server: TextView = itemView.findViewById(R.id.server)
         val description: TextView = itemView.findViewById(R.id.description)
+
 
     }
 
@@ -30,18 +37,25 @@ class DashboardAdapter(private var list:List<DataItem>?) : RecyclerView.Adapter<
         return list?.size ?: 0
     }
 
-//    fun setList(newList:List<DataItem>?){
-//        list = newList
-//        notifyDataSetChanged()
-//    }
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val itemViewPosition = list?.get(position)
-        holder.title.text = itemViewPosition?.name
-        holder.created_date.text = itemViewPosition?.createdAt
-        holder.server.text = itemViewPosition?.server
-        holder.description.text = itemViewPosition?.description
+        try {
+            holder.title.text = itemViewPosition?.name?.substring(0,10) + "..."
+            holder.created_date.text = Utils.getLocalFormat(itemViewPosition?.createdAt.toString())
+            holder.server.text = itemViewPosition?.server?.substring(6,19) + "..."
+            holder.description.text = itemViewPosition?.description?.substring(0,50) + "..."
+        } catch (e:Exception){
+            e.printStackTrace()
+        }
 
+        holder.deleteProject.setOnClickListener{
+
+            Utils.showDialogConfirmation(context,"Delete dashboard","are you sure to delete this dashboard ?", onPositiveClick = {
+                itemViewPosition?.id?.let { it1 -> viewModel.deleteDashboard(it1) }
+            }, onNegativeClick = {
+                return@showDialogConfirmation
+            })
+        }
     }
 }
