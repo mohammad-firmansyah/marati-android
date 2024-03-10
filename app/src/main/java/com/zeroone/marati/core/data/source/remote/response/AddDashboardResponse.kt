62202fb55ae29e425bebac5e2b50061548654c90
mohammad firmansyah <firmansmoh842@gmail.com>
2024-03-10 11:00:38 +0700

    fix : simplify response

--- app/src/main/java/com/zeroone/marati/core/data/source/remote/response/AddDashboardResponse.kt
+++ app/src/main/java/com/zeroone/marati/core/data/source/remote/response/AddDashboardResponse.kt
@@ -1,19 +1,9 @@
 package com.zeroone.marati.core.data.source.remote.response
 
 data class AddDashboardResponse(
-	val data: DataDashboard? = null,
+	val data: List<DataItem>? = null,
 	val isError: Boolean? = null,
 	val message: String? = null
 )
 
-data class DataDashboard(
-	val server: String? = null,
-	val password: String? = null,
-	val ownerId: String? = null,
-	val name: String? = null,
-	val description: String? = null,
-	val createdAt: String? = null,
-	val id: String? = null,
-	val username: String? = null
-)
 
