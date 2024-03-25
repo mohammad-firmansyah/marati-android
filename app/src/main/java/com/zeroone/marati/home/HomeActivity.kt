fcfc52cde9960befec80832a920e1dce23f65143
mohammad firmansyah <firmansmoh842@gmail.com>
2024-03-25 22:33:38 +0700

    add : addFragment and deleteFragment to manage fragments

--- app/src/main/java/com/zeroone/marati/home/HomeActivity.kt
+++ app/src/main/java/com/zeroone/marati/home/HomeActivity.kt
@@ -1,15 +1,16 @@
 package com.zeroone.marati.home
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.zeroone.marati.R
 import com.zeroone.marati.databinding.ActivityHomeBinding
-import com.zeroone.marati.home.Fragments.HomeFragment
-import com.zeroone.marati.home.Fragments.ModelFragment
-import com.zeroone.marati.home.Fragments.ProfilFragment
-import com.zeroone.marati.home.Fragments.SettingFragment
+import com.zeroone.marati.home.fragments.HomeFragment
+import com.zeroone.marati.home.fragments.ModelFragment
+import com.zeroone.marati.home.fragments.ProfilFragment
+import com.zeroone.marati.home.fragments.SettingFragment
 import com.zeroone.marati.core.ui.PreferenceManager
 import com.zeroone.marati.core.ui.ViewModelFactory
 import com.zeroone.marati.dataStore
@@ -26,6 +27,7 @@ class HomeActivity : AppCompatActivity() {
         val factory = ViewModelFactory.getInstance(this,pref)
         viewModel = ViewModelProvider(this,factory).get(HomeViewModel::class.java)
 //        supportActionBar?.hide()
+        window.statusBarColor  = Color.parseColor("#000000")
         replaceFragment(HomeFragment())
 
         binding.bottomBar.setOnItemSelectedListener {
@@ -45,11 +47,26 @@ class HomeActivity : AppCompatActivity() {
     }
 
 
-    private fun replaceFragment(fragment : Fragment){
+    fun replaceFragment(fragment : Fragment){
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_layout,fragment)
         fragmentTransaction.commit()
+    }
+    fun addFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.menu_frame_layout,fragment)
+        fragmentTransaction.addToBackStack(fragment.tag)
+        fragmentTransaction.commit()
+    }
+
+    fun deleteFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.remove(fragment)
+        fragmentTransaction.commit()
+        fragmentManager.popBackStack()
     }
 
 }
